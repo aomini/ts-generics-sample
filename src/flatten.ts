71060501c4e40f9c9a3b2e107b07e4869f30d126
Rakesh Shrestha <rak3sh.shrestha@gmail.com>
2024-11-27 20:49:15 +0545

    adds module detection to force so that no export is required

---
+++ src/flatten.ts
@@ -0,0 +1,24 @@
+// Question:
+/*
+  type Flatten<T> = {
+    // Implement here
+  };
+
+  type NestedArray = [number, [string, [boolean]]];
+  type Flattened = Flatten<NestedArray>; 
+  // Result: [number, string, boolean]
+*/
+
+type Flatten<T> = T extends (infer U)[] ? Flatten<U> : T;
+
+type NestedArray = [number, [string, [boolean]]];
+type Flattened = Flatten<NestedArray>;
+// Result: [number, string, boolean]
+
+/**
+  🚨 Explanation:
+  Infering a new generics helps to make use of that non existing type.
+  T extends (infer U)[] : T should always be an array if it is true then it is inferred as Array<U> 
+  if T is not an array then T itself is returned (flattened type)
+  And then the single type U is returned
+ */
